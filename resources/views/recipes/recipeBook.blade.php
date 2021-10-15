1acb211e3e62d2e4d29313b17c3bfbbff744d229
EvaRoets <eva_roets@hotmail.com>
2021-10-15 16:13:29 +0200

    Add basic structure recipe book

--- resources/views/recipes/recipeBook.blade.php
+++ resources/views/recipes/recipeBook.blade.php
@@ -1,1 +1,15 @@
-<?php
+@extends('layouts.layout')
+@section('content')
+    <p>This is the recipe book page</p>
+    <div class="recipe-container">
+        <div class="recipe-filter">
+
+        </div>
+        <div class="left-page">
+
+        </div>
+        <div class="right-page">
+
+        </div>
+    </div>
+@endsection
