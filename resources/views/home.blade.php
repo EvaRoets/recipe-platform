e8189d545318f5797adb1a7a9e188313c23487ff
EvaRoets <eva_roets@hotmail.com>
2021-10-15 16:19:43 +0200

    Add basic structure home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.layout')
 @section('content')
     <p>This is the home page</p>
+    <div class="home-container">
+        <div class="home-filter">
+
+        </div>
+        <div class="feed">
+
+        </div>
+    </div>
 @endsection
