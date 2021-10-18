3c80fd1fb4745767cbc1b36bbb3100e6e5801c3f
EvaRoets <eva_roets@hotmail.com>
2021-10-18 10:01:15 +0200

    Add recipe card for recipe book

--- resources/views/recipes/components/card.blade.php
+++ resources/views/recipes/components/card.blade.php
@@ -1,1 +1,14 @@
-<?php
+<div class="recipe-container">
+    <div class="card-gallery">
+{{--        TODO display image(s) from database --}}
+    </div>
+    <div class="card-title">
+{{--        TODO display title from database--}}
+    </div>
+    <div class="card-tags">
+{{--        TODO display tags from database--}}
+    </div>
+    <div class="card-buttons">
+        <button class="see">See recipe</button>
+    </div>
+</div>
