699dbfcf46424a01bd11c69a670fcc5aa51def61
EvaRoets <eva_roets@hotmail.com>
2021-10-18 11:20:48 +0200

    Add dummy data to recipe card home

--- resources/views/recipes/components/cardFeatured.blade.php
+++ resources/views/recipes/components/cardFeatured.blade.php
@@ -1,11 +1,20 @@
 <div class="recipe-container">
     <div class="card-gallery">
         {{--        TODO display image(s) from database --}}
+        <img src="../../../../public/css/images/shakshuka.jpeg" alt="recipe-picture(s)"/>
     </div>
     <div class="card-title">
         {{--        TODO display title from database--}}
+        <h4>Shakshuka</h4>
     </div>
     <div class="card-tags">
+        <badge>dinner</badge>
+        <badge>egg</badge>
+        <badge>tomato</badge>
+        <badge>middleeastern</badge>
+        <badge>vegetarian</badge>
+        <badge>healthy</badge>
+
         {{--        TODO display tags from database--}}
     </div>
     <div class="card-buttons">
