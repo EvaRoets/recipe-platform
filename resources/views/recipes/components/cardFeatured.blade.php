535be44e4b3587e38ebb4f55cc472187dd05f4f4
EvaRoets <eva_roets@hotmail.com>
2021-10-18 10:01:03 +0200

    Add recipe card for home

--- resources/views/recipes/components/cardFeatured.blade.php
+++ resources/views/recipes/components/cardFeatured.blade.php
@@ -1,1 +1,15 @@
-<?php
+<div class="recipe-container">
+    <div class="card-gallery">
+        {{--        TODO display image(s) from database --}}
+    </div>
+    <div class="card-title">
+        {{--        TODO display title from database--}}
+    </div>
+    <div class="card-tags">
+        {{--        TODO display tags from database--}}
+    </div>
+    <div class="card-buttons">
+        <button class="like">Like </button>
+        <button class="save">Save </button>
+    </div>
+</div>
