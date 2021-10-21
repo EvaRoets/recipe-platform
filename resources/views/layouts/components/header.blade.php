c688fd4a488a400d390ccb72ec78be65621eab1b
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-10-21 18:29:19 +0200

    Add styling to navbar

--- resources/views/layouts/components/header.blade.php
+++ resources/views/layouts/components/header.blade.php
@@ -6,26 +6,24 @@
         {{--    logo starts here--}}
         <div class="logo-container">
             <a href="#home">
-                <img class="logo" src={{ asset('images/logo/flavorly-icon.png') }} alt=""/></a>
+                <img class="logo" src={{ asset('images/logo/flavorly-full-crop.png') }} alt=""/>
+            </a>
         </div>
 
         {{--    search bar starts here--}}
-        <div class="searchbar">
-            <form action="">
-                <label>
+        <div class="search-container">
+{{--            <form action="">--}}
                     <input type="text" placeholder="Search..." name="search">
                     <button class="search-button" type="submit" value="submit">
                         <i class="fa fa-search"></i>
                     </button>
-
-
-                </label>
-            </form>
+{{--            </form>--}}
         </div>
-
         {{--    profile collapse starts here--}}
-        <div class="profile">
-            <button type="button" class="collapsible">Profile</button>
+        <div class="profile-container">
+            <button type="button" class="collapsible">
+                <img class="profile" src={{ asset('images/account.png') }} alt=""/>
+            </button>
             <div class="content">
                 <p>Lorem ipsum...</p>
             </div>
