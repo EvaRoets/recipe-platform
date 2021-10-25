d670f252ae8ee2dc241dd090a51f3c34cf86892f
Jens Rosseel <slaythehorizon.games@gmail.com>
2021-10-25 11:38:08 +0200

    Refactor controllers

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Controller;
 Route::get('/', [Controller::class, 'index'])->name('home');
 
 Route::get('/login', [Controller::class, 'login'])->name('login');
-Route::post('/checklogin', [Controller::class, 'checklogin']);
-Route::get('/successlogin', [Controller::class, 'successlogin']);
-Route::get('/logout', [Controller::class, 'logout']);
+Route::post('/checklogin', [SessionController::class, 'checklogin']);
+Route::get('/successlogin', [SessionController::class, 'successlogin']);
+Route::get('/logout', [SessionController::class, 'logout']);
 
 Route::get('recipeBook', function () {
     return view('recipeBook');
