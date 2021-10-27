13bc32cb2a6baf1a7cfbb6a685b11da316f8095b
Jens Rosseel <slaythehorizon.games@gmail.com>
2021-10-27 11:05:27 +0200

    Add hashing

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($pass)
+    {
+    $this->attributes['password'] = bcrypt($pass);
+    }
 }
