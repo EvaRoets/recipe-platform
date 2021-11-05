33ef7a7bda75aa2d1f8ee64ca31e0c905a9b0525
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-11-05 15:25:26 +0100

    Style footer

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,7 +1,6 @@
-<footer class="fixed-bottom">
-    <p style="text-align: center"> Copyright &copy;
+<footer class="footer-container">
+    <p> Copyright &copy;
         <script>document.write(new Date().getFullYear())</script>
         | Jens Rosseel | Eva Roets
     </p>
-
 </footer>
