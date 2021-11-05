ee7f7a29d48b0af0bbf603de6f424238b0a2e0f1
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-11-05 14:12:56 +0100

    Fix foreach loop for double card-featured-container and create bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,36 +3,42 @@
     <div class="home-container">
         <div class="feed">
             @foreach($posts as $key => $data)
-            <div class="card-featured-container">
-                <div class="card-title">
-                    <h2>{{ $data->title }}</h2>
+                <div class="card-featured-container">
+                    <div class="card-title">
+                        <h2>{{ $data->title }}</h2>
+                    </div>
+                    <div class="card-gallery">
+                        <div class="card-tags">
+                            @foreach(explode(',', $data->tags) as $tag)
+                                <badge>{{ $tag }}</badge>
+                            @endforeach
+                        </div>
+                        <div class="card-image-container">
+                            <img class="card-image" alt="recipe-picture(s)" src={{ asset($data->image) }} />
+                        </div>
+                        <div class="card-buttons">
+                            <a href={{ route("save", ['postid' => $data->id]) }}>
+                                <button class="save"><i class="fas fa-heart"></i></button>
+                            </a>
+                            <a href="">
+                                <button class="like"><i class="fas fa-bookmark"></i></button>
+                            </a>
+                            <a href={{ route("details", ['postid' => $data->id]) }}>
+                                <button class="view"><i class="fas fa-eye"></i></button>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-gallery">
-                    <div class="card-tags">
-                        @foreach(explode(',', $data->tags) as $tag)
-                            <badge>{{ $tag }}</badge>
-                        @endforeach
-                    </div>
-                    <div class="card-image-container">
-                        <img class="card-image" alt="recipe-picture(s)" src={{ asset($data->image) }} />
-                    </div>
-                    <div class="card-buttons">
-                        <a href={{ route("save", ['postid' => $data->id]) }}><button class="save"><i class="fas fa-heart"></i></button></a>
-                        <a href=""><button class="like"><i class="fas fa-bookmark"></i></button></a>
-                        <a href={{ route("details", ['postid' => $data->id]) }}><button class="view"><i class="fas fa-eye"></i></button></a>
-                    </div>
-                </div>
-            </div>
+            @endforeach
+
             <div class="create-container">
                 <div class="create"></div>
                 <button class="add"><i class="fas fa-plus-circle"></i></button>
             </div>
+        </div>
     </div>
     <script src="../../js/app.js"></script>
     <noscript>Your browser does not support JavaScript.</noscript>
-            @endforeach
-        </div>
-    </div>
 @endsection
 
 
